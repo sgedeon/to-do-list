829751dab10ea13a031c5f76cf8afa054403a09f
Sebastien Gedeon <sebastiengedeon@MacBook-Air-de-Sebastien.local>
2022-01-14 19:49:12 -0500

    ajout modules et classes

---
+++ js/classes.js
@@ -0,0 +1,10 @@
+export let object = class {
+  constructor (nom, importance, date, id, fait) {
+      this.nom = nom;
+      this.importance = importance;
+      this.date = date;
+      this.id = id;
+      this.fait = fait;
+  }
+}
+
